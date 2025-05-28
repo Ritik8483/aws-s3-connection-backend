89d2a96815d095a803b0b8609b59c4e970bfbc3b
Ritik Chauhan <ritik.chauhan@infomerics.com>
2025-05-28 12:40:14 +0530

    (feat): complete aws s3 get and put objects

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //install @aws-sdk/client-s3,npm i @aws-sdk/s3-request-presigner
 //import S3Client and S3RequestPresigner
+// https://www.youtube.com/watch?v=oAR3pahiCQk&list=PLr60VaMrue74TwtxNfMGteP4yaousjCNz
 
-const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 const client = new S3Client({
@@ -24,6 +25,17 @@ const getObjectSignedUrl = async (key) => {
   return url;
 };
 
+const putObjectSignedUrl = async (key,contentType) => {
+  const command = new PutObjectCommand({
+    Bucket: "ritikchauhanvatsbucket",
+    Key: key,
+    ContentType: contentType,
+  });
+
+  const url = await getSignedUrl(client, command);      //once we upload a object on a url it doesn't work again
+  return url;
+};
+
 (async () => {
   //   const hello = await getObjectSignedUrl('ritik');      //gives access denied
   const hello = await getObjectSignedUrl("Picture 21, Picture.jpeg"); //Correct object key in bucket , paste key from properties of Object
@@ -37,6 +49,14 @@ const getObjectSignedUrl = async (key) => {
   // https://ritikchauhanvatsbucket.s3.us-east-1.amazonaws.com/Picture%2021%2C%20Picture.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAUQ4L26EPJ6KL37NV%2F20250528%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250528T053021Z&X-Amz-Expires=900&X-Amz-Signature=ac9da2420b0c32dfac0aff0a1151abc41525f51258e850177ffc25a9b75b9552&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject
 })();
 
+(async () => {
+  const putUrl = await putObjectSignedUrl("newUniqueKey",'image/jpeg');
+  console.log("putUrl",putUrl);
+//   https://ritikchauhanvatsbucket.s3.us-east-1.amazonaws.com/newUniqueKey?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAUQ4L26EPJ6KL37NV%2F20250528%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250528T065925Z&X-Amz-Expires=900&X-Amz-Signature=d584ed2889f2e7f3d9ccc927de779769cbb6dac1eb7a1e92e9626668bdce510b&X-Amz-SignedHeaders=host&x-amz-checksum-crc32=AAAAAA%3D%3D&x-amz-sdk-checksum-algorithm=CRC32&x-id=PutObject
+})();
+
+//Same for DELETE AND LISTING
+
 //We create a user named as Ram on behalf of root user
 // IAM -> Users -> Create User (Ram)
 // Then Security Credentials -> Access Key -> Create Access key -> CLI -> Create access key -> Copy Access key
@@ -48,3 +68,7 @@ const getObjectSignedUrl = async (key) => {
 // put - insert
 
 //INSTALL AWS CLI
+
+
+// --------------
+// Now paste puturl in postman and select request type as PUT,Body as binary and upload your image .jpeg
